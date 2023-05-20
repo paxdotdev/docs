5c190ef2dcf83d829edc55597cf04831ca2d54cf
Zack Brown <zack@pax.rs>
2023-05-19 17:03:13 -0700

    update deployment for docs.pax.dev

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 set -e
 mdbook build
-aws --profile=inclination s3 sync --acl=public-read ./book/ s3://docs.pax.dev/
-aws --profile=inclination cloudfront create-invalidation --distribution-id=E2EYK7TMGOPCYY --paths "/*"
+aws --profile=pax s3 sync --acl=public-read ./book/ s3://docs.pax.dev/
+aws --profile=pax cloudfront create-invalidation --distribution-id=E2EYK7TMGOPCYY --paths "/*"
