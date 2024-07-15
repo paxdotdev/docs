02e1fa794642ae4f0e73dda70e631af5241b60c2
Zack Brown <zack@pax.dev>
2024-07-15 12:45:47 -0700

    remove version-controlled out dir

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 set -e
 npm run build
-aws --profile=pax s3 sync --acl=public-read ./out/ s3://docs.pax.dev/
+aws --profile=pax s3 sync --delete --acl=public-read ./out/ s3://docs.pax.dev/
 aws --profile=pax cloudfront create-invalidation --distribution-id=E2EYK7TMGOPCYY --paths "/*"
