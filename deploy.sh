b523530fbe9ec1d033d884ba5a6769b17663b003
Zack Brown <zack@pax.dev>
2024-08-23 18:13:07 +0700

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 set -e
-VERCEL_FORCE_NO_BUILD_CACHE=1 pnpm next export
+VERCEL_FORCE_NO_BUILD_CACHE=1 pnpm next build && pnpm next export
 aws --profile=pax s3 sync --delete --acl=public-read ./out/ s3://docs.pax.dev/
 aws --profile=pax cloudfront create-invalidation --distribution-id=E2EYK7TMGOPCYY --paths "/*"
